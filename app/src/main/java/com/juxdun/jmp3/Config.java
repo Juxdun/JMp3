281655cd563100eb33b1b147c86ddfa8095e6299
Juxdun <1018633103@qq.com>
2015-03-05 10:47:45 +0800

    搭建框架

---
+++ app/src/main/java/com/juxdun/jmp3/Config.java
@@ -0,0 +1,7 @@
+package com.juxdun.jmp3;
+
+/**
+ * Created by Juxdun on 2015/3/5.
+ */
+public class Config {
+}
---
+++ app/src/main/java/com/juxdun/jmp3/bo/MP3.java
@@ -0,0 +1,8 @@
+package com.juxdun.jmp3.bo;
+
+/**
+ * MP3实体类
+ * Created by Juxdun on 2015/3/5.
+ */
+public class MP3 {
+}
